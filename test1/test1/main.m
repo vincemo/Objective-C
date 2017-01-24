9c9765407e43fc9ef655ff79e15ac32112de232c
莫雨帆 <vincemo@126.com>
2017-01-24 16:10:00 +0800

    Create test 1

---
+++ test1/test1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  test1
+//
+//  Created by MMMOOO on 2017/1/24.
+//  Copyright © 2017年 MMMOOO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
